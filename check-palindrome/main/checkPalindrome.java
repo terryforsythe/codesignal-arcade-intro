2a4da2a6e7747515937833ceb57868b181f222f9
Terry Thompson <b.terry.thompson@gmail.com>
2017-05-16 19:49:38 -0400

    working logic for palindrome checker

--- check-palindrome/main/checkPalindrome.java
+++ check-palindrome/main/checkPalindrome.java
@@ -1,5 +1,19 @@
 package main;
 
-public class checkPalindrome {
+public class CheckPalindrome {
+
+	public static boolean checkPalindrome(String inputString) {
+
+		int length = inputString.length();
+
+		for (int index = 0; index < (length / 2); index++) {
+			 
+			if (inputString.charAt(index) != inputString.charAt((length - 1) - index)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 
 }
--- check-palindrome/test/checkPalindromeTest.java
+++ check-palindrome/test/checkPalindromeTest.java
@@ -1,5 +1,41 @@
 package test;
 
-public class checkPalindromeTest {
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.CheckPalindrome;
+
+public class CheckPalindromeTest {
+
+	private String inputString;
+
+	@Test
+	public void shouldReturnTrueWhenOneLetterString() {
+		inputString = "a";
+		boolean result = CheckPalindrome.checkPalindrome(inputString);
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void shouldReturnFalseWhenInitialLetterDoesNotMatchLastLetter() {
+		inputString = "abc";
+		boolean result = CheckPalindrome.checkPalindrome(inputString);
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void shouldReturnFalseWhenInitialTwoLettersDoNotMatchLastTwoLetters() {
+		inputString = "abab";
+		boolean result = CheckPalindrome.checkPalindrome(inputString);
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void shouldReturnTrueWhenLongPalindrome() {
+		inputString = "abcdefgfedcba";
+		boolean result = CheckPalindrome.checkPalindrome(inputString);
+		assertEquals(true, result);
+	}
 
 }
