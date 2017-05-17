7d56e6ab2b5a6cf9fc25c7e6a234fb653e89e0e6
Terry Thompson <b.terry.thompson@gmail.com>
2017-05-16 20:47:18 -0400

    organized class imports

--- check-palindrome/test/checkPalindromeTest.java
+++ check-palindrome/test/checkPalindromeTest.java
@@ -1,10 +1,8 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import main.CheckPalindrome;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CheckPalindromeTest {
 
