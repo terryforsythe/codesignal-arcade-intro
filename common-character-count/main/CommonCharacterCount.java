b6729a07e3b03dcd5267868a75b15e13098de696
Terry Thompson <b.terry.thompson@gmail.com>
2017-07-01 22:19:54 -0400

    some changes and refactoring

--- common-character-count/main/CommonCharacterCount.java
+++ common-character-count/main/CommonCharacterCount.java
@@ -5,16 +5,16 @@ public class CommonCharacterCount {
 	public static int commonCharacterCount(String s1, String s2) {
 
 		int count = 0;
+		
+		char[] string1 = s1.toLowerCase().toCharArray();
+		StringBuilder string2 = new StringBuilder(s2.toLowerCase());
 
-		StringBuilder stringOne = new StringBuilder(s1.toLowerCase());
-		StringBuilder stringTwo = new StringBuilder(s2.toLowerCase());
+		for (char string1Index : string1) {
+			for (int string2Index = 0; string2Index < string2.length(); string2Index++) {
 
-		for (int string1Index = 0; string1Index < stringOne.length(); string1Index++) {
-			for (int string2Index = 0; string2Index < stringTwo.length(); string2Index++) {
-
-				if (stringOne.charAt(string1Index) == stringTwo.charAt(string2Index)) {
+				if (string1Index == string2.charAt(string2Index)) {
 					count += 1;
-					stringTwo.deleteCharAt(string2Index);
+					string2.deleteCharAt(string2Index);
 					break;
 				}
 
