08032d32b5a90d13f3f00c0ea3b9b5f041d22abe
Terry Thompson <b.terry.thompson@gmail.com>
2017-08-13 21:02:30 -0400

    fixed .gitignore

--- is-lucky/main/IsLucky.java
+++ is-lucky/main/IsLucky.java
@@ -2,4 +2,27 @@ package main;
 
 public class IsLucky {
 
+	public static boolean isLucky(int n) {
+
+		String ticketNumber = Integer.toString(n);
+		int halfNumberLength = ticketNumber.length() / 2;
+
+		char[] firstHalfOfNumber = ticketNumber.substring(0, halfNumberLength).toCharArray();
+		char[] secondHalfOfNumber = ticketNumber.substring(halfNumberLength).toCharArray();
+
+		int firstHalfOfNumberSum = 0;
+		int secondHalfOfNumberSum = 0;
+
+		for (char digit : firstHalfOfNumber) {
+			firstHalfOfNumberSum += digit;
+		}
+
+		for (char digit : secondHalfOfNumber) {
+			secondHalfOfNumberSum += digit;
+		}
+
+		return firstHalfOfNumberSum == secondHalfOfNumberSum;
+
+	}
+
 }
--- is-lucky/test/IsLuckyTest.java
+++ is-lucky/test/IsLuckyTest.java
@@ -1,5 +1,45 @@
 package test;
 
+import org.junit.Test;
+import main.IsLucky;
+import static org.junit.Assert.assertEquals;
+
 public class IsLuckyTest {
+
+	int ticketNumber;
+
+	boolean luckyResult() {
+		return IsLucky.isLucky(this.ticketNumber);
+	}
+
+	@Test
+	public void shouldReturnTrueWhenFirstNumberEqualsSecondNumberInTwoDigitNumber() {
+		ticketNumber = 33;
+		assertEquals(true, luckyResult());
+	}
+
+	@Test
+	public void shouldReturnFalseWhenFirstNumberDoesNotEqualSecondNumberInTwoDigitNumber() {
+		ticketNumber = 54;
+		assertEquals(false, luckyResult());
+	}
+
+	@Test
+	public void shouldReturnTrueWhenFirstHalfOfNumberEqualsSecondHalfInFourDigitNumber() {
+		ticketNumber = 1524;
+		assertEquals(true, luckyResult());
+	}
+
+	@Test
+	public void shouldReturnTrueWhenFirstHalfOfNumberEqualsSecondHalfInSixDigitNumber() {
+		ticketNumber = 234162;
+		assertEquals(true, luckyResult());
+	}
+
+	@Test
+	public void shouldReturnFalseWhenTicketHasOddNumberOfDigits() {
+		ticketNumber = 23432;
+		assertEquals(false, luckyResult());
+	}
 
 }
